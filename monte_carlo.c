24d55aac5ad28f5b61b49edf9ed18c587fc6c416
Sunil4836 <84697502+Sunil4836@users.noreply.github.com>
2021-06-04 14:21:54 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -8,6 +8,21 @@ float mc_pi(int n ){
   for ( int i=0; i<n ;i++ ){
   float x = frandom();
   float y= frandom();
+float mc_pi(int n ){
+   int incircle = 0;
+   
+  for ( int i=0; i<n ;i++ ){
+  float x = frandom();
+  float y= frandom();
+  float  d= x*x + y*y;
+  if ( d <= 1 ){
+   incircle++;
+   }
+  }
+  float pi = 4*(float)incircle/n;
+   return pi;   
+  
+  }
   float  d= x*x + y*y;
   if ( d <= 1 ){
    incircle++;
